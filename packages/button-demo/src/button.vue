5aeb738d9b5f035407a1d44a01e260c4ab26b455
marui01 <marui01@ofo.com>
2018-02-26 15:10:52 +0800

    change version

---
+++ packages/button-demo/src/button.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <div class="button">{{btnText}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "button",
+    props:{
+        btnText:{
+            default: '按钮',
+            type: String
+        }
+    }
+}
+</script>
+
